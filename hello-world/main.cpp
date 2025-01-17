86cdd659cd0254fe9b69a4976b985373aa237c65
Gar Powell <gpowell@uinta1.com>
2025-01-16 19:28:21 -0700

    Update main.cpp

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,16 @@
+
 //
-// Name
-// Date
-// Hello World Programming Project
+// Name Gar Powell
+// Date 1/16/25
+// Your First C++ Program
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+```
