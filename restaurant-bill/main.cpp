559818adbb0c87434dcb7de319926d9782d1e10c
Gar Powell <gpowell@uinta1.com>
2025-01-25 01:22:06 +0000

    Ready to be graded.

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,20 +1,44 @@
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 1/24/25
 // Restaurant Bill Programming Project
 // COSC 1030
 //
 
-// Variable declarations
+#include <iostream>
+using namespace std;
 
-// Constants for the tax rate and tip percentage
+int main ()
+{
+    // Variable declarations
+    float restaurantBill;
+    float taxAmount;
+    float tipAmount;
+    float totalBill;
 
-// Get the amount of the purchase.
+    // Constants for the tax rate and tip percentage
+    const double tax_rate = 0.0675;
+    const double tip_percentage = 0.20;
 
-// Calculate the sales tax.
+    // Get the amount of the purchase.
+    cout << "Enter the cost of the meal" << endl;
+	cout << "Then hit enter" << endl;
+	cin >> restaurantBill;
 
-// Calculate the tip.
+    // Calculate the sales tax.
+    taxAmount = restaurantBill * tax_rate;
 
-// Calculate the total of the sale.
+    // Calculate the tip.
+    tipAmount = restaurantBill * tip_percentage;
 
-// Print information about the sale.
+    // Calculate the total of the sale.
+    totalBill = restaurantBill + tipAmount + taxAmount;
+
+    // Print information about the sale.
+    cout << endl << "For a restaurant bill of $" << restaurantBill << endl
+        << "The tax is $" << taxAmount << endl 
+		<< "The tip is $" << tipAmount << endl
+        << "The total is $" << totalBill << endl;
+
+    return 0;
+}
