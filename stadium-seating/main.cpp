fddca9fbb50ac856e3d503aef59f0f49f5d3985d
Gar Powell <gpowell@uinta1.com>
2025-02-04 23:54:13 +0000

    Ready to be graded

--- stadium-seating/main.cpp
+++ stadium-seating/main.cpp
@@ -1,7 +1,61 @@
+
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 2/3/25
 // Stadium Seating Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+    // Variable declarations
+    int classASeats;
+    int classBSeats;
+    int classCSeats;
+    int allSeats;
+    double totalClassA;
+    double totalClassB;
+    double totalClassC;
+    double totalAll;
+
+    // Constants for each seat price
+    const int costClassA = 15;
+    const int costClassB = 12;
+    const int costClassC = 9;
+
+    // Get the amount of each class of seat sold.
+    cout << "Enter the amount of class A seats sold" << endl;
+	cout << "Then hit enter" << endl;
+	cin >> classASeats;
+
+    cout << "Enter the amount of class B seats sold" << endl;
+	cout << "Then hit enter" << endl;
+	cin >> classBSeats;
+
+    cout << "Enter the amount of class C seats sold" << endl;
+	cout << "Then hit enter" << endl;
+	cin >> classCSeats;
+
+    // Calculate the total amount from each class of seat sold.
+    totalClassA = classASeats * costClassA;
+    totalClassB = classBSeats * costClassB;
+    totalClassC = classCSeats * costClassC;
+
+    // Calculate the total amount from all seats sold.
+    allSeats = classASeats + classBSeats + classCSeats;
+    totalAll = totalClassA + totalClassB + totalClassC;
+
+    // Print information about the seats sold and income generated.
+    cout << fixed << showpoint;
+    cout << setprecision(2);
+    cout << endl << "You sold " << classASeats << " class A seats and made $" << totalClassA << endl
+        << "You sold " << classBSeats << " class B seats and made $" << totalClassB << endl 
+		<< "You sold " << classCSeats << " class C seats and made $" << totalClassC << endl 
+        << "You sold " << allSeats << " total seats and made $" << totalAll << endl;
+
+    return 0;
+}
--- woody-furniture-company/main.cpp
+++ woody-furniture-company/main.cpp
@@ -1,7 +1,59 @@
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 2/4/25
 // Woody Furniture Company Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main ()
+{
+    // Variable declarations
+    int AmericanColonial;
+    int Modern;
+    int FrenchClassical;
+    int allChairs;
+    double totalAmericanColonial;
+    double totalModern;
+    double totalFrenchClassical;
+    double totalAll;
+
+    // Constants for cost of each chair.
+    const int costAmericanColonial = 85.00;
+    const int costModern = 57.50;
+    const int costFrenchClassical = 127.75;
+
+    // Get the amount of each chair sold.
+    cout << "Enter amount of American Colonial chairs sold." << endl;
+	cout << "Then hit enter." << endl;
+	cin >> AmericanColonial;
+
+    cout << "Enter amount of Modern chairs sold." << endl;
+	cout << "Then hit enter." << endl;
+	cin >> Modern;
+
+    cout << "Enter amount of French Classical chairs sold." << endl;
+	cout << "Then hit enter." << endl;
+	cin >> FrenchClassical;
+
+    // Calculate the total amount from each style of chair sold.
+    totalAmericanColonial = AmericanColonial * costAmericanColonial;
+    totalModern = Modern * costModern;
+    totalFrenchClassical = FrenchClassical * costFrenchClassical;
+
+    // Calculate the total chairs sold and amount from total chairs sold.
+    allChairs = AmericanColonial + Modern + FrenchClassical;
+    totalAll = totalAmericanColonial + totalModern + totalFrenchClassical;
+
+    // Print information about the seats sold and income generated.
+    cout << fixed << showpoint;
+    cout << setprecision(2);
+    cout << endl << "You sold " << AmericanColonial << " American Colonial chairs and made $" << totalAmericanColonial << endl
+        << "You sold " << Modern << " Modern chairs and made $" << totalModern << endl 
+		<< "You sold " << FrenchClassical << " French Classical chairs and made $" << totalFrenchClassical << endl 
+        << "You sold " << allChairs << " total seats and made $" << totalAll << endl;
+
+    return 0;
+}
