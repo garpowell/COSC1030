e58223655169b86eadccf05cd9552b741b899605
Gar Powell <gpowell@uinta1.com>
2025-02-17 17:58:16 +0000

    Ready to be graded.

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,29 @@
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 2/17/25
 // Calories Burned Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // Variable declarations
+    double caloriesBurned;
+    int minutes;
+
+    // Constants declarations.
+    const double calPerMin = 3.6;
+
+    // For loop to calculate the calories burned.
+    for (minutes = 5; minutes <= 30; minutes +=5)
+    {
+        caloriesBurned = minutes * calPerMin;
+        cout << endl << "You have been going for "<< minutes 
+        << " minutes and have burned " << caloriesBurned << " calories so far."<< endl;
+    }
+
+    return 0;
+}
