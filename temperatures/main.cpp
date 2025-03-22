5cd39425c5815c64b06d29ecdef1b65bb9128431
Gar Powell <gpowell@uinta1.com>
2025-03-22 17:23:47 +0000

    Ready to be graded.

--- temperatures/main.cpp
+++ temperatures/main.cpp
@@ -1,7 +1,111 @@
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 3/21/25
 // Temperatures
 // COSC 1030
 //
+
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+// Functions
+void getTemperatures(int[], int);
+double averageTemperature(int[], int);
+int maxTemperature(int[], int);
+int minTemperature(int[], int);
+
+int main()
+{
+    // Variable declarations
+    int SIZE = 0; 
+    double temperatureAverage = 0;
+    int maxIndex = 0;
+    int minIndex = 0;
+
+    // Get the number of temperatures to be entered.
+    cout << "How many temperatures do you have to enter?\n";
+    cin >> SIZE;
+    while (SIZE > 50)
+        {
+            cout << "The maxumum number of temperatures can not be more that 50. \n";
+            cout << "Please enter a number 50 or less. \n";
+            cin >> SIZE;
+        }
+
+    // Declare the array type.
+    typedef int tempArray[SIZE];
+
+    // Initialize the array for temperatures.
+    tempArray temperatures;
+
+    // Call the functions.
+    getTemperatures(temperatures, SIZE);
+    temperatureAverage = averageTemperature(temperatures, SIZE);
+    maxIndex = maxTemperature(temperatures, SIZE);
+    minIndex = minTemperature(temperatures, SIZE);
+
+    // Print the average, max and nin temperatures.
+    cout << "The maximum temperature was "<< temperatures[maxIndex] << "."<< endl;
+    cout << "The mimimum temperature was " << temperatures[minIndex] << "."<< endl;
+
+    cout << fixed << showpoint;
+    cout << setprecision(2);
+    cout << "The average temperature was " << temperatureAverage << "." << endl;
+
+    return 0;
+}
+
+// Function to get the temperatures.
+void getTemperatures(int temperature[], int SIZE)
+{
+    cout << "Enter the amount of sales for each type of salsa sold for the past month.\n";
+    for (int index = 0; index < SIZE; index++)
+    {
+     // Get the temperatures.
+     cout << "Enter the " << (index + 1) << " temperature\n";
+     cin >> temperature[index];
+    }
+
+}
+
+// Average temperatures function.
+double averageTemperature(int temperatures[], int SIZE)
+{
+    double total_temperatures = 0;
+    double average_temperatures = 0;
+    for (int index = 0; index < SIZE; index++)
+        total_temperatures = total_temperatures + temperatures[index];
+    
+    average_temperatures = total_temperatures/SIZE;
+
+    return average_temperatures;
+}
+
+// Max temperature function.
+int maxTemperature(int temperatures[], int SIZE)
+{
+    int max_index = 0;
+    for (int index = 1; index < SIZE; index++)
+        if (temperatures[max_index] < temperatures[index])
+            max_index = index;
+
+    return max_index;
+}
+
+// Min temperature function.
+int minTemperature(int temperatures[], int SIZE)
+{
+    int min_index = 0;
+    for (int index = 1; index < SIZE; index++)
+        if (temperatures[min_index] > temperatures[index])
+            min_index = index;
+
+    return min_index;
+}
+   
+
 
