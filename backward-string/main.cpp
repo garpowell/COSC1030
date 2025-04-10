53848dc8cb2f4d3b222178c2e956b9e5524346fa
Gar Powell <gpowell@uinta1.com>
2025-04-10 01:37:41 +0000

    Ready to be graded

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,42 @@
 //
-// Name
-// Date
+// Name Gar Powell
+// Date 4/9/25
 // Backward String Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// Function prototype
+void sortLine(char *);
+
+int main()
+{
+    // Initialize variables.
+    const int SIZE = 80;
+    char line[SIZE];
+    
+    // Get the string you want to convert.
+    cout << "Enter a word or phrase you would like to see backward.\n";
+    cout << "No more than " << (SIZE - 1) << " characters.\n";
+    cin.getline(line, SIZE);
+
+    // Call the function to print the string backward.
+    sortLine(line);
+
+    return 0;
+}
+
+// Function to reorder and print the string.
+void sortLine(char *line)
+{
+    int count = 0;
+    int endIndex;
+    endIndex = strlen(line) -1;
+    // Print the string backward
+    for (count = endIndex; count >= 0; count--)
+        cout << line[count];
+    cout << endl;   
+}
